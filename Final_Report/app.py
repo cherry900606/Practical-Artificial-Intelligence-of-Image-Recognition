e48323e54e5cd1035564e70d5afce44dedb57733
chenchen <cherry900606@gmail.com>
2023-07-23 15:48:23 +0800

    remove useless code

--- Final_Report/app.py
+++ Final_Report/app.py
@@ -32,7 +32,6 @@ if canvas_result.image_data is not None:
         urllib.request.urlretrieve('https://github.com/cherry900606/Practical-Artificial-Intelligence-of-Image-Recognition/raw/master/Final_Report/model_2.h5', 'model_2.h5')
     model = keras.models.load_model('model_2.h5', compile=False)
     drawing = cv.cvtColor(canvas_result.image_data, cv.COLOR_BGR2GRAY)
-    cv.imshow('show',drawing)
     im = cv.resize(drawing, (32, 32))
     label = model.predict(im.reshape(1, 32, 32))
     pred = np.argmax(label)
